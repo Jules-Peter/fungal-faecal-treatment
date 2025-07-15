5471c3e6affdd4c40901e115b220279a7bd96e33
Jules-Peter <jupeter@ethz.ch>
2025-07-15 08:26:51 +0200

    s

--- R/01-data_download.R
+++ R/01-data_download.R
@@ -5,10 +5,10 @@ library("tidyverse")
 library("googlesheets4")
 
 ###Reading the files from google drive 
-bacteria_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/1ebVGUMhg8nP3Gw3SB31uc8O0oHBcpB5spuHSfflGB3Y/edit?usp=drive_link")
-experiment_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/1MomeP7Ze-__ckRGYnVc0vEGAYguyDYRCSwLjdqlNdo0/edit?usp=drive_link")
-faeces_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/14D7pKH48NwMo57FGLHlRrPSq0tUb_e_OCZGtBfb3pfQ/edit?usp=drive_link")
-growth_speed_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/1_HANDFM0bu00BEnb98vUIvHjrrEzJmIhUPWiKvE4Pw0/edit?usp=drive_link")
-inoculum_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/1HOQQ1kHausy6wV6syHrEa-nih1btRHIXwTIr__fGrGA/edit?usp=drive_link")
+bacteria_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/1HEGDEres6dtpPC9Hlje8mq1gSiPV5tv6HVcqMCTcm7Q/edit?usp=drive_link")
+experiment_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/1LAyaN5P7wxgKNc9Acbh59-fOWWVrjkTOBtTCqOmxAc8/edit?usp=drive_link")
+faeces_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/15zEkXV1cOHz2bjWpk8h_SMbINThnYcgxCV30DCnaMOo/edit?usp=drive_link")
+growth_speed_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/14VSVuUM32Q5M2XNoXCCtOmAFMrl_rKPtyenp6XVz_dg/edit?usp=drive_link")
+inoculum_1_1 <- read_sheet("https://docs.google.com/spreadsheets/d/1A6j-FTdwOlPrr494TVFlIqkjl4RCOjVuNB03TEnBH2M/edit?usp=drive_link")
 
 ###Writing the files in git hup directory follow up
--- R/02-data_cleaning.R
+++ R/02-data_cleaning.R
@@ -6,79 +6,34 @@
 library("tidyverse")
 library("googlesheets4")
 
-######## Formatting the growth speed data showing are size change for each day#########
-glimpse(growth_speed_1_1)
+######## Formatting the growth speed data showing are size change for 7, 14, and 21 days post inoculation (dpi)#########
 
-growth_speed_transformed <- growth_speed_1_1 |>
-### Adding dpi calculated from starting date and picture date
+exp_growth_speed <- experiment_1_1 |>
+  left_join(growth_speed_1_1)|>
+  ### Adding dpi calculated from starting date and picture date
   mutate(dpi = as.numeric(date -starting_date,
-                          units = "days"))|>
-  select(id_treatment, dpi, area_size)|>
-### Make table wide putting all measured area sizes to the corresponding treatment choosing dpi as variable
-  pivot_wider(names_from = dpi,
-              values_from = area_size)
+                          units = "days"))
+#separate the columns that are constant per experiment_id.
+#    We'll use distinct() to make sure we only keep one row for these
+ 
+exp_growth_speed_base <- exp_growth_speed  |>
+  select(id_treatment, id_inoc, id_faeces, `weight_0dpi`, `weight_14dpi`) |>
+  distinct()
+  
+#Pivot the 'area size' column based on 'dpi'.
+#create new columns like area_size_0, area_size_7, etc.
+exp_area_size_pivot <- exp_growth_speed |>
+  select(id_treatment, dpi, `area_size`) |>
+  drop_na(area_size)|>
+  pivot_wider(names_from = dpi,values_from = `area_size`,
+    names_prefix = "area_size_")
 
-### change from area size per day to area size difference per day
-# Define the column names to select (as characters because they start with numbers)
-#To calculate difference between the time point, create two vectors to calculate the difference between days to select
-current_day_cols <- as.character(1:9)
-previous_day_cols <- as.character(0:8)
-
-### Select the two sets of columns
-df_current <- growth_speed_transformed |> 
-  select(all_of(current_day_cols))
-df_previous <- growth_speed_transformed |> 
-  select(all_of(previous_day_cols))
-
-### Subtract the data frames element-wise
-diff_values <- df_current - df_previous
-
-### Rename the columns of the result
-names(diff_values) <- paste0("diff_", current_day_cols) # e.g., diff_1, diff_2
-
-###Combine id column with the differences
-growth_speed_area_increase <- growth_speed_transformed |>
-  select(id_treatment) |>
-  bind_cols(diff_values)
-print(growth_speed_area_increase)
-
-### join tables of the growth speed data
-exp_growth_speed_inoculum_faeces <- experiment_1_1 |>
-  left_join(growth_speed_area_increase) |>
-  left_join(inoculum_1_1) |>
-  left_join(faeces_1_1)|>
-  ###Add column with inoculum age
-  mutate(inoculum_age = as.numeric(starting_date - production_date,
-                                   units = "days"))|>
-  ###Add column with mean_growth_speed
-  mutate(mean_growth_speed = rowMeans(across(starts_with("diff_"))))
-glimpse(exp_growth_speed_inoculum_faeces)
-
-### select coloumns according to research questions ( by inoculum substrate, species....)
-growth_speed_analysis_long <- exp_growth_speed_inoculum_faeces |>
-    # Gather columns diff_1 through diff_9
-  pivot_longer(
-    cols = starts_with("diff_"), # Selects all columns starting with "diff_"
-    names_to = "time_period",   # New column for the original column name (e.g., "diff_1")
-    values_to = "difference") |>   # New column for the numeric difference value)
-  # Extract the numeric part from the time_period column for the x-axis
-  mutate(
-    time_step = parse_number(time_period) # Extracts number (1 from "diff_1", etc.)
-  )|>
-group_by(species)|>  # Group by species
-  summarise(mean_diff_values = mean(parse_number(time_period), na.rm = TRUE), # Calculate the mean
-            sd_diff_values = sd(parse_number(time_period), na.rm = TRUE)) # Calculate the standard deviation
-
-
-### For mean growth speed and dependence on incoulum age and substrate 
-growth_speed_analysis <- exp_growth_speed_inoculum_faeces|>
-  select(species, material, inoculum_age, mean_growth_speed)
-glimpse(growth_speed_analysis)
-
+# Join the base information with the pivoted area sizes.
+# left_join to ensure all experiment_ids from the base info are kept.
+growth_speed_analysis <- left_join(exp_growth_speed_base, exp_area_size_pivot, by = "id_treatment")
 
 ### Save growth speed as csv in processed
-write_csv(growth_speed_analysis, "data/processed/growth_speed_analysis.csv")  
-write_csv(growth_speed_analysis_long, "data/processed/growth_speed_analysis_long.csv")
+write_csv(growth_speed_analysis, "data/processed/growth_speed_analysis.csv")
 
 ############ Preparing tables for bacterial analysis ###############
 bacteria_conc <- bacteria_1_1|>
@@ -86,7 +41,7 @@ bacteria_conc <- bacteria_1_1|>
   mutate(cfu_enterococcus_end = enterococcus_counted*dilution_enterococcus/sample_weight)
 
 faeces_conc <- faeces_1_1|>
-  mutate(cfu_ecoli_start = e_coli_counted*dilution_factor_ecoli/sample_weight)|>
+  mutate(cfu_ecoli_start = e_coli_counted_1*dilution_factor_ecoli_1/sample_weight_1)|>
   mutate(cfu_enterococcus_start = enterococcus_counted*dilution_factor_enterococcus/sample_weight)
 
 exp_bacteria <-left_join(experiment_1_1, bacteria_conc, by="id_treatment")
